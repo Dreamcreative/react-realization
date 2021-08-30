edd08fbb7ec83121245fce33c5890fabb05ae4cd
maxiao <m543438924@163.com>
2021-08-30 22:38:11 +0800

    feat:删除react-reconciler-temp

--- packages/react-reconciler-temp/ReactFiberExpirationTime.js
+++
@@ -1,9 +0,0 @@
-export const NoWork = 0;
-// 最大的31 bit integer
-export const Sync = 1073741823;
-export const Batched = Sync - 1;
-const UNIT_SIZE = 10;
-const MAGIC_NUMBER_OFFSET = Batched - 1;
-export function msToExpirationTime(ms) {
-  return MAGIC_NUMBER_OFFSET - ((ms / UNIT_SIZE) | 0)
-}
